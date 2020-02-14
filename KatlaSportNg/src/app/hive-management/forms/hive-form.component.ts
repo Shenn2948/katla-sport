f528dea5e7d24dae78ff62ed05a11ff30bcb973b
Shenn2948 <shenn2948@gmail.com>
2020-02-15 00:14:59 +0300

    Merge step12.Add the ability to create a new hive, edit and delete an existing one

--- KatlaSportNg/src/app/hive-management/forms/hive-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-form.component.ts
@@ -36,14 +36,22 @@ export class HiveFormComponent implements OnInit {
   }
   
   onSubmit() {
+    if (this.existed) {
+      this.hiveService.updateHive(this.hive).subscribe(() => this.navigateToHives());
+    } else {
+      this.hiveService.addHive(this.hive).subscribe(() => this.navigateToHives());
+    }
   }
 
   onDelete() {
+    this.hiveService.setHiveStatus(this.hive.id, true).subscribe(() => this.hive.isDeleted = true);
   }
 
   onUndelete() {
+    this.hiveService.setHiveStatus(this.hive.id, false).subscribe(() => this.hive.isDeleted = false);
   }
 
   onPurge() {
+    this.hiveService.deleteHive(this.hive.id).subscribe(() => this.navigateToHives());
   }
 }
--- KatlaSportNg/src/app/hive-management/lists/hive-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-list.component.ts
@@ -11,7 +11,7 @@ export class HiveListComponent implements OnInit {
 
   hives: HiveListItem[];
 
-  constructor(private hiveService: HiveService) { }
+  constructor(private hiveService: HiveService) {}
 
   ngOnInit() {
     this.getHives();
@@ -19,15 +19,5 @@ export class HiveListComponent implements OnInit {
 
   getHives() {
     this.hiveService.getHives().subscribe(h => this.hives = h);
-  }
-
-  onDelete(hiveId: number) {
-    var hive = this.hives.find(h => h.id == hiveId);
-    this.hiveService.setHiveStatus(hiveId, true).subscribe(c => hive.isDeleted = true);
-  }
-
-  onRestore(hiveId: number) {
-    var hive = this.hives.find(h => h.id == hiveId);
-    this.hiveService.setHiveStatus(hiveId, false).subscribe(c => hive.isDeleted = false);
   }
 }
